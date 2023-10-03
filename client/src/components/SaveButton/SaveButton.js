479afda074a7c5379c3de78dc223a39640387455
lozzolloz <124617157+lozzolloz@users.noreply.github.com>
2023-10-03 20:17:04 +0100

    tweaking spacing 3

--- client/src/components/SaveButton/SaveButton.js
+++ client/src/components/SaveButton/SaveButton.js
@@ -3,7 +3,7 @@ import "./SaveButton.css";
 export default function SaveButton({ onClick, theme }) {
   return (
     <div className="save-button">
-      <button className={"save-button--"+theme} onClick={onClick}>Save image</button>
+      <button className={"save-button--"+theme} onClick={onClick}>Save to<br></br>deskstop</button>
     </div>
   );
 }
